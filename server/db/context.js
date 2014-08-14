68b2a725997b9e267d413001361147c5b40772db
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-08-14 09:41:12 -0500

    Remove db from GitHub

--- server/db/context.js
+++ server/db/context.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize'), 
     sequelize = new Sequelize('db', 'pizza-place', 'pizzapassword', {
         dialect: 'sqlite',
-        storage: './db/db.sqlite'
+        storage: 'db.sqlite'
     });
 
 module.exports = sequelize;
